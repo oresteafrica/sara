6427fd3cfc77c71a01481a89281b2f410c656c7a
Oreste <oreste@parlatano.org>
2016-09-17 22:21:01 +0200

    marker development

--- js/controlpanel.js
+++ js/controlpanel.js
@@ -1,20 +1,37 @@
 $( document ).ready(function() {
 var curfile = window.location.href ;
 var curdir = curfile.substring(0, curfile.lastIndexOf('/'));
-var divmap = $('#map')[0] ;
 // alert(window.location.origin+'\n'+window.location.hostname+'\n'+curfile+'\n'+curdir);
 
 init_tree(curdir);
+var map = init_map('#map');
 
-
-var map = new google.maps.Map(divmap, {
-zoom: 6,
-center: new google.maps.LatLng(-19,36),
-mapTypeId: google.maps.MapTypeId.ROADMAP
+$('#tree').children('div').eq(0).children('button').click(function(){
+	var label = $('#tree').children('div').eq(0).children('label').html();
+	$('#form').html('<h2>'+label+'</h2>');
 });
 
 
 
+
+//----------------------------------------------------------------------------------------------------------------------
+function mark(map, lat, lng, title) {
+	var myLatlng = new google.maps.LatLng(lat, lng);
+	var marker = new google.maps.Marker({
+		position: myLatLng,
+		title: title
+	});
+	marker.setMap(map);
+}
+//----------------------------------------------------------------------------------------------------------------------
+function init_map(div) {
+	var map = new google.maps.Map($(div)[0], {
+		zoom: 6,
+		center: new google.maps.LatLng(-19,36),
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	});
+	return map;
+}
 //----------------------------------------------------------------------------------------------------------------------
 function init_tree(url) {
 	$.ajax({
@@ -29,7 +46,8 @@ function init_tree(url) {
 				$(this).children('ul').toggle('slow');
 			});
 			$('span').click(function(){
-				$('#tree').children('div').eq(0).children('label').html($(this).text() + ' ('+this.id+')');
+				var hup = $(this).parent().parent().parent().children('span').text();
+				$('#tree').children('div').eq(0).children('label').html(hup+'<br />'+$(this).text() + ' ('+this.id+')');
 			});
 		},
 		error: function(a,b,c){ alert('erro ajax\na = ' + a.responseText + '\nb = ' + b + '\nc = ' + c ); },
---
+++ php/sara_unit.php
@@ -0,0 +1,30 @@
+<?php
+$localhosts = array(
+    '127.0.0.1',
+    'localhost',
+	'::1'
+);
+if(in_array($_SERVER['REMOTE_ADDR'], $localhosts)) {
+ini_set('display_errors', '1');
+error_reporting(E_ALL | E_STRICT);
+}
+$id_unit = $_GET['n'];
+$ini_array = parse_ini_file('../../cron/sara.ini');
+$sdsn = $ini_array['sdsn'];
+$user = $ini_array['user'];
+$pass = $ini_array['pass'];
+$opts = array(
+	PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+);
+try {
+    $db = new PDO($sdsn, $user, $pass, $opts);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    die('Problemas de conexão à base de dados:<br/>' . $e);
+}
+$sql = 'SELECT lat,lon FROM units_coord WHERE id_unit = '.$id_unit;
+$tabquery = $db->query($sql);
+$tabquery->setFetchMode(PDO::FETCH_ASSOC);
+$row = $tabquery->fetch();
+echo $row['lat'].';'.$row['lon'];
+?>
