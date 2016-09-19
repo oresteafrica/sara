4582f084158a43b9bdc8700ef453c0fb514093bd
Oreste <oreste@parlatano.org>
2016-09-19 13:44:35 +0200

    init location

--- js/controlpanel.js
+++ js/controlpanel.js
@@ -46,8 +46,22 @@ function init_tree(url) {
 				$(this).children('ul').toggle('slow');
 			});
 			$('span').click(function(){
-				var hup = $(this).parent().parent().parent().children('span').text();
-				$('#tree').children('div').eq(0).children('label').html(hup+'<br />'+$(this).text() + ' ('+this.id+')');
+                var lugar = {};
+				var huup_t = $(this).parent().parent().parent().parent().parent().children('span').text();
+				var huup_i = $(this).parent().parent().parent().parent().parent().children('span').attr('id');
+				var hup_t = $(this).parent().parent().parent().children('span').text();
+				var hup_i = $(this).parent().parent().parent().children('span').attr('id');
+                var h_t = $(this).text();
+                var h_i = this.id;
+                if (huup_t == '' && hup_t == '' ) {
+                    lugar.provname = $(this).text();
+                    lugar.provid = this.id;
+                }
+				$('#tree').children('div').eq(0).children('label').html(
+                    huup_t+' ('+huup_i+') <br />'+
+                    hup_t+' ('+hup_i+') <br />'+
+                    h_t + ' ('+h_i+')'
+                );
 			});
 		},
 		error: function(a,b,c){ alert('erro ajax\na = ' + a.responseText + '\nb = ' + b + '\nc = ' + c ); },
